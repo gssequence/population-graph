4e844c5162a670859ca7ac1c5efb12f43a8491d1
livewing.net <dev@livewing.net>
2021-04-17 00:18:53 +0900

    Add 'params' argument to resas.get() function
    クエリパラメータを resas.get() 関数へ渡せるよう、引数 params を追加。

--- src/utils/resas.ts
+++ src/utils/resas.ts
@@ -12,18 +12,23 @@ if (typeof apiKey !== 'string' || apiKey.length === 0) {
  * 環境変数 `RESAS_API_KEY` に API キーが設定されている必要があります。
  * ステータスコードが 2xx 以外の場合、 Promise は reject されます。
  * @param path API エンドポイントのパス。 `'/api/v1/prefectures'` などを指定します。
+ * @param params HTTP GET クエリパラメータ。 `{ prefCode: '1', cityCode: '-' }` などを指定します。
  * @param retry リトライ回数。既定値は `3` です。
  * @param retrySleep リトライ時の待機時間 (ミリ秒) 。既定値は `1000` です。
  * @returns JSON 値に resolve される Promise オブジェクト。
  */
 export const get = async (
   path: `/api/v1/${string}`,
+  params: { [key: string]: string } = {},
   retry = 3,
   retrySleep = 1000
 ): Promise<JSONType> => {
   try {
+    const paramStrBody = new URLSearchParams(params).toString();
+    const paramStr = paramStrBody.length === 0 ? '' : `?${paramStrBody}`;
+
     // fetch
-    const res = await fetch(`${apiBaseAddress}${path}`, {
+    const res = await fetch(`${apiBaseAddress}${path}${paramStr}`, {
       method: 'GET',
       headers: { 'X-API-KEY': apiKey }
     });
@@ -52,6 +57,6 @@ export const get = async (
     // リトライ
     await new Promise(resolve => setTimeout(resolve, retrySleep));
     if (retry === 0) throw err;
-    return await get(path, retry - 1, retrySleep);
+    return await get(path, params, retry - 1, retrySleep);
   }
 };
